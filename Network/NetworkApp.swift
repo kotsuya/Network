578a7b3dbd8d7902d8bfee0abd8193f54d3cacbb
Yoo <kotsuya00@gmail.com>
2025-11-08 07:43:23 +0900

    Initial Commit

---
+++ Network/NetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkApp.swift
+//  Network
+//
+//  Created by YOO on 2025/11/08.
+//
+
+import SwiftUI
+
+@main
+struct NetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
